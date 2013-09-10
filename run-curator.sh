9d6bed284d7b7897aa72fd058bf1af80a5ba2896
Charles Erwin <greymeister@gmail.com>
2013-09-09 22:46:35 -0700

    adding crontab example; updating script to run from working dir; removing unecessary requirement

--- run-curator.sh
+++ run-curator.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 set -e
+# Get the location of the script to make sure sqlite3 DB is in working dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
+# Run script with first arg from STDIN
 ./curator.py $1
