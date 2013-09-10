280eb44352f8b5e6343a655b838d1e236eaee838
Charles Erwin <greymeister@gmail.com>
2013-09-09 21:48:37 -0700

    making bucket name random(er)

--- curator.py
+++ curator.py
@@ -24,24 +24,48 @@ class CuratorDao:
     def mark_entry_visited( self, url ):
         self.__insert_url_into_db( url )
 
+    def get_s3_bucket( self ):
+        return self.__select_s3_bucket_from_db()
+
+    def set_s3_bucket( self, name ):
+        self.__insert_s3_bucket_into_db( name ) 
+
     def __select_item_url_from_db( self, item_url ):
-        self.__create_table_if_necessary()
+        self.__create_unique_links_table_if_necessary()
         c = self.conn.cursor()
         t = (item_url,)
         c.execute( 'select link from unique_links where link=?', t)
         return c.fetchone()
 
     def __insert_url_into_db( self, item_url ):
-        self.__create_table_if_necessary()
+        self.__create_unique_links_table_if_necessary()
         c = self.conn.cursor()
         t = (self.__get_date(), item_url,)
         c.execute( 'insert into unique_links values (?, ?)', t )
         self.conn.commit()
 
-    def __create_table_if_necessary(self) :
+    def __create_unique_links_table_if_necessary(self) :
         c = self.conn.cursor()
         c.execute( '''create table if not exists unique_links
               (curated_at text, link text)''')
+
+    def __select_s3_bucket_from_db( self ):
+        self.__create_s3_bucket_table_if_necessary()
+        c = self.conn.cursor()
+        c.execute( 'select bucket_name from s3_bucket' )
+        return c.fetchone()
+
+    def __insert_s3_bucket_into_db( self, name ):
+        self.__create_s3_bucket_table_if_necessary()
+        c = self.conn.cursor()
+        t = (name,)
+        c.execute( 'insert into s3_bucket values (?)', t )
+        self.conn.commit()
+
+    def __create_s3_bucket_table_if_necessary(self) :
+        c = self.conn.cursor()
+        c.execute( '''create table if not exists s3_bucket 
+              (bucket_name text)''')
 
     def __get_db_connection(self):
             return sqlite3.connect(self.db_name)
@@ -89,16 +113,28 @@ class CuratorPublisher:
         if not boto.config.has_section('Credentials'): 
             raise Exception("Boto configuration not found! Cannot Continue.")
         s3 = boto.connect_s3()
-        bucket = s3.create_bucket(self.__generate_bucket_name())
+        bucket = s3.create_bucket(self.__get_bucket_name())
         k = Key(bucket)
         k.key = self.url
         k.set_contents_from_string(content,headers={'Content-Type': 'application/rss+xml'})
         k.set_acl('public-read')
         feed_url = k.generate_url(0, query_auth=False, force_http=True)
         print "Your RSS Subscription is available at:\n%s" % feed_url 
-        
+       
+    def __get_bucket_name(self):
+        dao = CuratorDao()
+        bucket_name = dao.get_s3_bucket()
+        if bucket_name is None:
+            bucket_name = self.__generate_bucket_name()
+            dao.set_s3_bucket(bucket_name)
+            return bucket_name
+        else:
+            return bucket_name[0]
+
     def __generate_bucket_name(self):
-        return  "curator%s" % hashlib.sha1(os.environ["USER"]).hexdigest()
+        suffix = 'greymeistercurator'
+        noise = os.urandom(1024)
+        return  ''.join([hashlib.sha1(noise).hexdigest(), suffix])
 
 # Script Function
 def get_feed_url_from_args():
